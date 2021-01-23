2543b8a2b07eb4783cb198624f6182c995bf303a
mahsa-nvr <mahsaanvari2@gmail.com>
2021-01-23 16:30:59 +0330

    second commit

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component.jsx';
 
+import hat from './../../../src/assets/img/hat.png';
+import sneakers from './../../../src/assets/img/sneakers.png';
+import jackets from './../../../src/assets/img/jacket.jpg';
+import womens from './../../../src/assets/img/women.png';
+import mens from './../../../src/assets/img/men.jpg';
+
 import './directory.style.scss'; 
 
 
@@ -10,29 +16,42 @@ class Directory extends React.Component {
         this.state = {
             sections: [{
                 title: 'hats',
-                imageUrl: 'https://i.ibb.co/cvpntl1/hats.png',
+                imageUrl: `${hat}`,
                 id:1
             },{
                 title: 'jackets',
-                imageUrl: 'https://i.ibb.co/cvpntl1/hats.png',
+                imageUrl: `${jackets}`,
                 id:2
             },{
-                title: 'womens',
-                imageUrl: 'https://i.ibb.co/cvpntl1/hats.png',
+                title: 'sneakers',
+                imageUrl: `${sneakers}` ,
                 id:3
+            },{
+                title: 'womens',
+                imageUrl: `${womens}`,
+                size:'large',
+                id:4
+            },{
+                title: 'mens',
+                imageUrl: `${mens}`,
+                size:'large',
+                id:5
             }]
         }
     }
 
     render() {
         return (
+            <>       
             <div className="directory-menu">
+                
               {
-                  this.state.sections.map(( {title, imageUrl , id }) => (
-                      <MenuItem key={id} title={title} />
+                  this.state.sections.map(( {title, imageUrl , id , size }) => (
+                      <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                   ))
               }
             </div>
+            </>
         )
     }
 }
